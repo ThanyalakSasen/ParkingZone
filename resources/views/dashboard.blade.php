ca081fd8863ab6ef3b42ed7bf9e34fbf6c305b25
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2024-09-30 11:49:44 +0700

    Add icon browser.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="{{ asset('icon.png') }}" sizes="32x32">
     <link rel="stylesheet" href="{{ asset('dashboard.css') }}">
     <title>จองที่จอดรถ</title>
 
--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="{{ asset('icon.png') }}" sizes="32x32">
     <title>ชำระเงินสำหรับการจองที่จอดรถ - โปรโมชั่น</title>
     <link rel="stylesheet" href="{{ asset('payment.css') }}">
 </head>
